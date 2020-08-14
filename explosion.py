8321a606e7aa38a0d92f9af052aed629e504b3f0
nuolio1 <69449169+nuolio1@users.noreply.github.com>
2020-08-14 08:49:26 +0800

    Add files via upload

--- explosion.py
+++ explosion.py
@@ -1,3 +1,9 @@
-form mcpi.minecraft import Minecraft
-mc=Minecraft.creaft()
-while True
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+while True:
+    hits=mc.events.pollBlockHits()
+    if len(hits)>0:
+        hit=hits[0]
+        x,y,z=hit.pos.x,hit.pos.y,hit.pos.z
+        big_magic=mc.createExplosion(x,y,z,50)
